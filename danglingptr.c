3e24d90bf869ef51ead41f0c8f3202f703e9bf1d
Liam Ellison <liam.ellison.codes@gmail.com>
2025-03-26 19:55:34 -0400

    post ch14 program

---
+++ danglingptr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("creating ptr\n");
+    int *ptr = (int *)calloc(5, sizeof(int));
+
+    printf("gonna free its mem now lol...\n");
+    free(ptr);
+
+    printf("hey guess what\n");
+    scanf("%d", ptr);
+    
+    int* chkbt = ptr;
+    printf("%d", *chkbt);
+
+}
